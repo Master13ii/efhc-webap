dac56dcb36f3960b5de4df042eb6bbf01093ac75
EFHC <xonicsx@gmail.com>
2025-04-25 16:07:31 +0300

    Create nftData.js

---
+++ data /nftData.js
@@ -0,0 +1,4 @@
+export const nftData = [
+  { id: 1, name: 'EFHC Token #1', description: 'Первый токен EFHC', imageUrl: '/images/logo.png' },
+  { id: 2, name: 'EFHC Token #2', description: 'Второй токен EFHC', imageUrl: '/images/logo.png' }
+];
